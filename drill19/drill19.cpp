b72c9b6b3edbfdc791094d984121e2cf4c0b615f
barackhu1 <barackhu1@gmail.com>
2022-04-21 12:33:03 +0200

    drill19 update

--- drill19/drill19.cpp
+++ drill19/drill19.cpp
@@ -1,39 +1,85 @@
+/*	1. Define template<typename T> struct S { T val; }; .
+	
+	2. Add a constructor, so that you can initialize with a T .
+	
+	3. Define variables of types S<int> , S<char> , S<double> , S<string> , and
+S<vector<int>> ; initialize them with values of your choice.
+	
+	4. Read those values and print them.
+
+	5. Add a function template get() that returns a reference to val .
+
+	6. Put the definition of get() outside the class.
+
+	7. Make val private.
+
+	8. Do 4 again using get() .
+
+	9. Add a set() function template so that you can change val .
+
+	10. Replace set() with an S<T>::operator=(const T&) . Hint: Much simpler
+than §19.2.5.
+
+	11. Provide const and non- const versions of get() .
+
+	12. Define a function template<typename T> read_val(T& v) that reads from
+cin into v .
+	
+	13. Use read_val() to read into each of the variables from 3 except the
+S<vector<int>> variable.
+
+	14. Bonus: Define input and output operators ( >> and << ) for vector<T> s.
+For both input and output use a { val, val, val } format. That will allow
+read_val() to also handle the S<vector<int>> variable.*/
+
 #include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+//1.feladat
+
 template<typename T>
 struct S {
-	S (T vv = 0) : val{vv}{};
-	T& get();
-	void set(T new_t) {val = new_t;}
-	void operator=(const T& S);
-	const T& get() const;
+	/*2.feladat*/S (T vv = 0) : val{vv}{};
+	/*5.feladat*/T& get();
+	/*9.feladat*/void set(T new_t) {val = new_t;}
+	/*10.feladat*/void operator=(const T& s){val = s;};
+	/*11.feladat*/const T& get() const;
+
+	//7.feladat
 private:
 	T val;
 };
+
+//5. és 6.feladat
 
 template<typename T>
 T& S<T>::get(){
 	return val;
 }
 
+//11.feladat
+
 template<typename T>
 const T& S<T>::get() const{
 	return val;
 }
 
-template<typename T>
+/*template<typename T>
 void S<T>::operator=(const T& s){
 	val = s;
-}
+}*/
+
+//12.feladat
 
 template<typename T>
 void read_val(T& v){
 	cin >> v;
 }
+
+//14.feladat
 
 template<typename T>
 ostream& operator<<(ostream& os, vector<T>& v){
@@ -46,30 +92,32 @@ ostream& operator<<(ostream& os, vector<T>& v){
 			os << ", ";
 		}
 	}
-	os << "}" << endl;
+	os << "}";
 	return os;
 }
 
 template<typename T>
 istream& operator>>(istream& is, vector<T>& v){
 	char ch = 0;
-	is >> ch;
-	if (ch != '{')
-	{
-		is.unget();
-		return is;
-	}
-	for (T val; is >> val;)
-	{
-		v.push_back(val);
-		is >> ch;
-		if (ch != ',') break;
-	}
-	return is;
+    is >> ch;
+    if (ch != '{') {
+        is.unget();
+        return is;
+    }
+
+    for (T val; is >> val; ) {
+        v.push_back(val);
+        is >> ch;
+        if (ch != ',') break;
+    }
+
+    return is;
 }
 
 int main(){
 	
+	//3.feladat
+
 	S<int> s;
 	S<int> si {37};
 	S<char> sc {'c'};
@@ -77,27 +125,34 @@ int main(){
 	S<string> ss{"Hello"};
 	S<vector<int>> svi {vector<int>{1,1,2,3,5,8}};
 
+	//4. és 8.feladat
+
 	cout << "S<int> : " << s.get() << endl;
 	cout << "S<int> : " << si.get() << endl;
 	cout << "S<char> : " << sc.get() << endl;
 	cout << "S<double> : " << sd.get() << endl;
 	cout << "S<string> : " << ss.get() << endl;
-	//cout << "S<vector<int>> : " << svi.val << endl;
+	cout << "S<vector<int>> : " << svi.get() << endl;
 
+	/* vector régi kiiratása
 	cout << "S<vector<int>> : ";
 
 	for (auto& a: svi.get())
 	{
 		cout << a << ' ';
-	}
+	}*/
 
-	cout << endl;
+	//8.feladat
 
 	sc.set('s');
 	cout << "S<char> : " << sc.get() << endl;
 
+	//10.feladat
+
 	sd = 42.1;
 	cout << "S<double> : " << sd.get() << endl;
+
+	//13.feladat
 
 	int ii;
 	read_val(ii);
@@ -114,6 +169,8 @@ int main(){
 	cout << "S<int> : " << si2.get() << endl;
 	cout << "S<double> : " << sd2.get() << endl;
 	cout << "S<string> : " << str.get() << endl;
+
+	//14.feladat
 
 	cout << "Add a vector in this format {val, val, val}: ";
 	vector<int> vint;
